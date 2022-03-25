9570d49b7b6fa038b5a9aa5415f0847612dbdac4
Steve Riesenberg <sriesenberg@vmware.com>
2022-03-25 12:56:24 -0500

    Fixed interceptor

--- samples/angular-client/src/app/interceptors/gateway.interceptor.ts
+++ samples/angular-client/src/app/interceptors/gateway.interceptor.ts
@@ -8,6 +8,10 @@ export class GatewayInterceptor implements HttpInterceptor {
   constructor() {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (req.url.startsWith('http')) {
+      return next.handle(req);
+    }
+
     const newReq = req.clone({
       url: 'http://127.0.0.1:8080' + req.url,
       withCredentials: true
