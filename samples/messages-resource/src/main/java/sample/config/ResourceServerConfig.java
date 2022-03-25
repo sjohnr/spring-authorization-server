190484cbcf272af9e5f5f9e900863b1669d4ac69
Steve Riesenberg <sriesenberg@vmware.com>
2022-03-25 12:55:49 -0500

    Removed CORS from resource server

--- samples/messages-resource/src/main/java/sample/config/ResourceServerConfig.java
+++ samples/messages-resource/src/main/java/sample/config/ResourceServerConfig.java
@@ -35,8 +35,6 @@ public class ResourceServerConfig {
 				.authorizeRequests()
 					.mvcMatchers("/messages/**").access("hasAuthority('SCOPE_message.read')")
 					.and()
-			.cors()
-				.and()
 			.oauth2ResourceServer()
 				.jwt();
 		return http.build();
