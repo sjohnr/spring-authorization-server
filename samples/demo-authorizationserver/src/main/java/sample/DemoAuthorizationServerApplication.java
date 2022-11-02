27f4161530bbe8350d238abc311ee067bb90345e
Steve Riesenberg <sriesenberg@vmware.com>
2022-11-02 11:51:12 -0500

    Create demo-authorizationserver project

---
+++ samples/demo-authorizationserver/src/main/java/sample/DemoAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAuthorizationServerApplication.class, args);
+	}
+
+}
