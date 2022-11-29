d8f5cbefe73460fae1d2fc382107b59b77551424
Steve Riesenberg <sriesenberg@vmware.com>
2022-11-29 13:30:04 -0600

    (Reset back to Getting Started)

--- samples/messages-resource/src/main/java/sample/config/ResourceServerConfig.java
+++ samples/messages-resource/src/main/java/sample/config/ResourceServerConfig.java
@@ -38,7 +38,7 @@ public class ResourceServerConfig {
 					.requestMatchers("/messages/**").hasAuthority("SCOPE_message.read")
 					.and()
 			.oauth2ResourceServer()
-				.opaqueToken();
+				.jwt();
 		return http.build();
 	}
 	// @formatter:on
